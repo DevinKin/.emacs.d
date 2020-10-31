0e0597d4feed2bc4331537703cb459fb833f38e0
devinkin <devinkin@163.com>
2020-10-31 12:16:05 +0800

    lsp-python configuration

--- lisp/prog/init-python.el
+++ lisp/prog/init-python.el
@@ -1,8 +1,17 @@
-(use-package lsp-jedi
+;; (use-package lsp-jedi
+;;   :ensure t
+;;   :config
+;;   (with-eval-after-load "lsp-mode"
+;;     (add-to-list 'lsp-disabled-clients 'pyls)
+;;     (add-to-list 'lsp-enabled-clients 'jedi)))
+
+(use-package lsp-python-ms
   :ensure t
-  :config
-  (with-eval-after-load "lsp-mode"
-    (add-to-list 'lsp-disabled-clients 'pyls)
-    (add-to-list 'lsp-enabled-clients 'jedi)))
+  :init (setq
+	 lsp-python-ms-executable "/usr/bin/mspyls"
+	 lsp-python-ms-auto-install-server t)
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-python-ms)
+                         (lsp))))  ; or lsp-deferred
 
 (provide 'init-python)
